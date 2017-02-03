89c728e231e08dfc34e5c6b37abec322d71e83ac
JASCValentine <jasc.valentine@gmail.com>
2017-02-03 11:42:53 +0800

    Update Kata.js

--- javascript/Kata.js
+++ javascript/Kata.js
@@ -10,53 +10,34 @@ function ListNode(data, next = null) {
 };
 
 function flatten(root) {
-	if(!root) return null;
+  if (!root) {
+    return null;
+  }
   
-  let arr = traverse(root);
-  let uniqueArray = [];
-  
-  // eliminate the duplicate elements
-  for(let ele of arr){
-  
-  	if(uniqueArray.indexOf(ele) == -1){
-    	uniqueArray.push(+ele);
+  const tree = (root, set) => {
+    if (root) {
+      list(root.value, set);
+      tree(root.left, set);
+      tree(root.right, set);
     }
+  };
+  const list = (head, set) => {
+    if (head) {
+      set.add(head.data);
+      list(head.next, set);
+    }
+  };
+  
+  let set = new Set();
+  tree(root, set);
+  let arr = Array.from(set).sort((a, b) => a - b);
+  
+  let head = new ListNode(arr.shift());
+  let temp = head;
+  for (let i of arr) {
+    temp.next = new ListNode(i);
+    temp = temp.next;
   }
   
-  // sort
-  uniqueArray.sort((a, b) => {return a-b});
-  
-  // to list
-  return toList(uniqueArray);
-};
-
-function traverse(root){
-	let valueArray = [], leftArray = [], rightArray = [];
-  
-  // get all elements from value
-  let value = root.value;
-  while(value){
-  	valueArray.push(value.data);
-    value = value.next;
-  }
-  
-  // get all elements from left
-	if(root.left){
-  	leftArray = traverse(root.left);
-  }
-  
-  // get all elements from left
-  if(root.right){
-  	rightArray = traverse(root.right);
-  }
- 
- 	// get all elements
-  return valueArray.concat(leftArray).concat(rightArray);
-};
-
-function toList(arr){
-	if(arr.length == 0) return null;
-	let l = new ListNode(arr.shift());
-  l.next = toList(arr);
-  return l;
+  return head;
 };
